9617891cac21bd9e62bb50f79c830e7920009ecf
Terry Ott <Terry.Ott@gmail.com>
2025-09-08 21:19:49 +0000

    SO MUCH WIN NO CATALOG

--- drive_distribution_by_mfr.py
+++ drive_distribution_by_mfr.py
@@ -1,45 +1,98 @@
-import json
-import logging
-import pyiceberg
-import pyiceberg.catalog
+import argparse
+import pandas
+import pathlib
+import pyiceberg.table
+import time
+
+import drive_stats_iceberg_table
 
 
-def _load_catalog() -> pyiceberg.catalog.Catalog:
+def _parse_args() -> argparse.Namespace:
+    arg_parser = argparse.ArgumentParser()
 
-    # Use REST Catalog (exposed by Nessie)
-    catalog_properties: dict[str, str] = {
-        'type'                  : 'rest',
-        'uri'                   : 'http://localhost:19120/iceberg',
-        'warehouse'             : 's3://drivestats-iceberg',
-        's3.access-key-id'      : '0045f0571db506a0000000017',
-        's3.secret-access-key'  : 'K004Fs/bgmTk5dgo6GAVm2Waj3Ka+TE',
-        's3.endpoint'           : 'https://s3.us-west-004.backblazeb2.com',
-        's3.region'             : 'us-west-004',
-    }
-    
-    catalog: pyiceberg.catalog.Catalog = pyiceberg.catalog.load_catalog("backblaze_b2", **catalog_properties)
+    default_s3_endpoint: str = "https://s3.us-west-004.backblazeb2.com"
+    default_b2_bucket_name: str = "drivestats-iceberg"
+    default_b2_region: str = "us-west-004"
+    default_table_path: str = "drivestats"
 
-    return catalog
+    arg_parser.add_argument("--s3-endpoint",
+                            default=default_s3_endpoint,
+                            help=f"S3 Endpoint (default: \"{default_s3_endpoint}\")")
+
+    arg_parser.add_argument("--b2-region",
+                            default=default_b2_region,
+                            help=f"B2 Region (default: \"{default_b2_region}\")")
+
+    arg_parser.add_argument("--bucket-name",
+                            default=default_b2_bucket_name,
+                            help=f"B2 Bucket Name (default: \"{default_b2_bucket_name}\")")
+
+    arg_parser.add_argument("--table-path",
+                            default=default_table_path,
+                            help=f"B2 Bucket Table Path (default: \"{default_table_path}\")")
+
+    arg_parser.add_argument("b2_access_key",
+                            help="Backblaze B2 Access Key" )
+    arg_parser.add_argument("b2_secret_access_key",
+                            help="Backblaze B2 Secret Access Key" )
+
+    return arg_parser.parse_args()
 
 
 def _main() -> None:
-    logging.basicConfig(level=logging.DEBUG)
-    catalog: pyiceberg.catalog.Catalog = _load_catalog()
+    args: argparse.Namespace = _parse_args()
 
-    print(json.dumps(catalog.properties, indent=2))
+    # What's latest schema of the Backblaze drive stats?
+    print("\nFetching current schema from Apache Iceberg table hosted in Backblaze B2...")
+    operation_begin_time: float = time.perf_counter()
+    current_schema_uri: str = drive_stats_iceberg_table.current_metadata_file_s3_uri(
+        args.b2_access_key,
+        args.b2_secret_access_key,
+        args.s3_endpoint,
+        args.bucket_name,
+        args.table_path )
+    operation_end_time: float = time.perf_counter()
 
-    print( f"Namespaces: {catalog.list_namespaces()}")
+    base_filename: str = pathlib.Path(current_schema_uri).name
 
-    # Try to open drivestats table
-    table = catalog.load_table( "default.drivestats" )
-    print( table.schema )
-    
-    # Check out StaticTable that reads a table straight from metadata, completely bypassing catalog.  You have my
-    #   attention. Use s3 to walk the dir and get latest version of metadata, and we can skip these shenanigans
+    print(f"\tCurrent schema: {base_filename}")
+    print(f"\tTime to retrieve schema from Backblaze B2: {operation_end_time - operation_begin_time:.03f} seconds")
 
-    # Looks like Backblaze writer script uses namespace of "default": https://github.com/backblaze-b2-samples/drivestats2iceberg
+    # See if we can open the static table and then we're done for the day
 
+    # Need to pass properties so they know how to find that S3 URL
+    schema_properties: dict[str, str] = {
+        "s3.endpoint"           : args.s3_endpoint,
+        "s3.region"             : args.b2_region,
+        "s3.access-key-id"      : args.b2_access_key,
+        "s3.secret-access-key"  : args.b2_secret_access_key,
+    }
 
+    print("\nOpening Backblaze Drive Stats Apache Iceberg table hosted in Backblaze B2...")
+    operation_begin_time: float = time.perf_counter()
+    static_table: pyiceberg.table.StaticTable = drive_stats_iceberg_table.open_drive_stats_table(
+        args.s3_endpoint,
+        args.b2_region,
+        args.b2_access_key,
+        args.b2_secret_access_key,
+        current_schema_uri
+    )
+    operation_end_time: float = time.perf_counter()
+    print(f"\tStatic table opened at s3://{args.bucket_name}/{args.table_path}")
+    print(f"\tTime to open table: {operation_end_time - operation_begin_time:.03f} seconds")
+
+    print("\nDoing table scan of date, model so we can get full drive mfr distribution...")
+    operation_begin_time: float = time.perf_counter()
+    scan_dataframe: pandas.DataFrame = static_table.scan(
+        selected_fields=('date', 'model'),
+        # limit=1000,
+    ).to_pandas()
+
+    operation_end_time: float = time.perf_counter()
+    print(f"\tTime to scan table: {operation_end_time - operation_begin_time:.03f} seconds")
+
+    print("Dataframe:")
+    print(scan_dataframe)
 
 
 if __name__ == "__main__":
--- drive_stats_iceberg_table.py
+++ drive_stats_iceberg_table.py
@@ -1,5 +1,7 @@
 import argparse
+import pyiceberg.table
 import s3fs
+import time
 
 
 def current_metadata_file_s3_uri( b2_access_key: str,
@@ -30,16 +32,40 @@ def current_metadata_file_s3_uri( b2_access_key: str,
     return latest_metadata_json_file
 
 
+def open_drive_stats_table(s3_endpoint: str,
+                           b2_region: str,
+                           b2_access_key: str,
+                           b2_secret_access_key: str,
+                           current_schema_uri: str ) -> pyiceberg.table.StaticTable:
+
+    schema_properties: dict[str, str] = {
+        "s3.endpoint"           : s3_endpoint,
+        "s3.region"             : b2_region,
+        "s3.access-key-id"      : b2_access_key,
+        "s3.secret-access-key"  : b2_secret_access_key,
+    }
+
+    static_table: pyiceberg.table.StaticTable = pyiceberg.table.StaticTable.from_metadata(
+        current_schema_uri, schema_properties)
+
+    return static_table
+
+
 def _parse_args() -> argparse.Namespace:
     arg_parser = argparse.ArgumentParser()
 
     default_s3_endpoint: str = "https://s3.us-west-004.backblazeb2.com"
+    default_b2_region: str = "us-west-004"
     default_b2_bucket_name: str = "drivestats-iceberg"
     default_table_path: str = "drivestats"
 
     arg_parser.add_argument("--s3-endpoint",
                             default=default_s3_endpoint,
                             help=f"S3 Endpoint (default: \"{default_s3_endpoint}\")")
+
+    arg_parser.add_argument("--b2-region",
+                            default=default_b2_region,
+                            help=f"B2 Region (default: \"{default_b2_region}\")")
 
     arg_parser.add_argument("--bucket-name",
                             default=default_b2_bucket_name,
@@ -61,15 +87,27 @@ def _parse_args() -> argparse.Namespace:
 def _main():
     args: argparse.Namespace = _parse_args()
 
-    # Let's see if we can find some metadata
-    latest_metadata_json_file: str = current_metadata_file_s3_uri(
+    current_schema_uri: str = current_metadata_file_s3_uri(
         args.b2_access_key,
         args.b2_secret_access_key,
         args.s3_endpoint,
         args.bucket_name,
         args.table_path )
 
-    print(f"Latest metadata file: {latest_metadata_json_file}")
+    print(f"Latest metadata file: {current_schema_uri}")
+
+    print("\nOpening Backblaze Drive Stats Apache Iceberg table hosted in Backblaze B2...")
+    drive_stats_table = open_drive_stats_table(
+        args.s3_endpoint,
+        args.b2_region,
+        args.b2_access_key,
+        args.b2_secret_access_key,
+        current_schema_uri
+    )
+
+    print(drive_stats_table.schema())
+
+
 
 
 if __name__ == '__main__':
